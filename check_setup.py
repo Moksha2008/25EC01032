19b4a68531bf2ee8d8132cf4e6bc78ae2a4f4084
Moksha2008 <25ec01032@iitbbs.ac.in>
2026-08-07 10:12:30 +0530

    Update setup

---
+++ check_setup.py
@@ -0,0 +1,13 @@
+import sys
+import numpy
+import pandas
+import matplotlib
+import scipy
+
+print("Python version:", sys.version)
+print("NumPy version:", numpy.__version__)
+print("Pandas version:", pandas.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("SciPy version:", scipy.__version__)
+print("Setup check: everything is good, yay !!!")
+print("minor change 1, lets go!!")
